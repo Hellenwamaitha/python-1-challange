9cc2a794c891f4d97a638bf611da3bd1c11572d2
hellenwamaitha <hellenwama254@gmail.com>
2023-08-20 21:30:44 +0300

    update challenge 3

--- Constant.py
+++ Constant.py
@@ -1,4 +1,16 @@
-def   constant_value(string):
-    constant_of_substring=()
-    for value,element in enumerate(string):
-        
+def constant_value(string):
+    vowel = ("aeiou")
+    current_value = 0
+    max_value = 0
+    
+    for char in string:
+        if char not in vowel:
+            current_value += ord(char) - ord('a') + 1
+        else:
+            max_value = max(max_value, current_value)
+            current_value = 0
+    
+    max_value = max(max_value, current_value)
+    
+    return max_value
+
