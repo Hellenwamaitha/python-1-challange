582c76ab94631ba2c5fb84a61b1933375f18cc7c
hellenwamaitha <hellenwama254@gmail.com>
2023-08-24 10:28:20 +0300

    updated work

--- Number.py
+++ Number.py
@@ -1,7 +1,8 @@
+# Use the if statement to check the numbers
 def two_numbers(a, b, c):
     if (a > 0 and b > 0) or (a > 0 and c > 0) or (b > 0 and c > 0):
         return True
     else:
         return False
-    
-    print(two_numbers(1,2,-8))
+
+print(two_numbers(1, 2, -8))
--- Time.py
+++ Time.py
@@ -1,12 +1,11 @@
+# Use the if statement to check the time 
+
 def convert_to_24_hour(hour, minute, period):
-    #use the conditional statements
     if period == "am" and hour == 12:
         hour = 0
     elif period == "pm" and hour != 12:
         hour += 12
     
-    return f"{hour:02d}{minute:02d}"
+    return f"{hour:02d}:{minute:02d}"
 
-
-
-print(convert_to_24_hour(7,29,"pm")) 
+print(convert_to_24_hour(7, 29, "pm"))
